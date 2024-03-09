447f47791c77f536ed947e1b17463d3b16ff80dc
amreus <harleyd68@gmail.com>
2024-03-09 11:59:12 -0500

    update

--- _sketches/basic/sketch.js
+++ _sketches/basic/sketch.js
@@ -1,8 +1,12 @@
 let angle = 0;
+let radius;
 
 function setup() {
     canvas = createCanvas(400, 400);
     canvas.parent('sketch')
+    radius = createSlider(10, 100, 10);
+    radius.parent('sketch')
+    radius.position(10, 10)
 }
 
 function draw() {
@@ -10,6 +14,6 @@ function draw() {
     translate(width/2, height/2)
     x = 100 * cos(angle);
     y = 100 * sin(angle);
-    circle(x, y, 100)
+    circle(x, y, radius.value())
     angle += 0.01;
 }
